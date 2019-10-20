d74a85bc1336d0a10dc85067f19fbb66ede57bfe
Pavel Ivanov <funkpaws@MacBook-Pro-Pavel.local>
2019-10-20 15:07:58 +0300

    Scripted auto-commit on change (2019-10-20 15:07:58) by gitwatch.sh

--- FlightEntry.java
+++ FlightEntry.java
@@ -8,9 +8,64 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import javax.xml.crypto.Data;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class FlightEntry implements Writable {
     private int airportId;
     private int delay;
     private boolean canceled;
-    public final static int CANCELED = 19
+    public final static int CANCELED = 19;
+    public final static int DEST_AIRPORT_ID = 14;
+    public final static int ARR_DELAY_NEW = 18;
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public int getAirportId() {
+        return airportId;
+    }
+
+    public FlightEntry(String string) {
+        String[] params = string.split(",");
+        airportId = Integer.parseInt(params[DEST_AIRPORT_ID]);
+        canceled = (((int) Double.parseDouble(params[CANCELED])) == 1);
+
+        if (!canceled && !params[ARR_DELAY_NEW].equals("")) {
+            delay = (int) Double.parseDouble(params[ARR_DELAY_NEW]);
+        } else {
+            delay = 0;
+        }
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(airportId);
+        dataOutput.writeInt(delay);
+        dataOutput.writeBoolean(canceled);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        airportId = dataInput.readInt();
+        delay = dataInput.readInt();
+        canceled = dataInput.readBoolean();
+    }
+
+    @Override
+    public String toString() {
+        return airportId + "," + delay + "," + canceled;
+    }
+}
+
+
+
+
 }
