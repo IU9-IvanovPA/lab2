3f92381b3d5202f87b257b417e1f34e7e29b1b79
Pavel Ivanov <funkpaws@MacBook-Pro-Pavel.local>
2019-10-20 14:55:52 +0300

    Scripted auto-commit on change (2019-10-20 14:55:52) by gitwatch.sh

--- FlightEntry.java
+++ FlightEntry.java
@@ -1,4 +1,16 @@
 package funkpaws.hadoop.lab2;
 
-public class FlightEntry {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FlightEntry implements Writable {
+    private int airportId;
+    private int delay;
+    private boolean canceled;
+    public final static int CANCELED = 19
 }
