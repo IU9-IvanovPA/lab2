5ac349a9b5b1f2cf7f4d819d0f600f482019a436
Pavel Ivanov <funkpaws@MacBook-Pro-Pavel.local>
2019-10-20 16:04:57 +0300

    Scripted auto-commit on change (2019-10-20 16:04:56) by gitwatch.sh

--- AirportEntry.java
+++ AirportEntry.java
@@ -1,4 +1,58 @@
 package funkpaws.hadoop.lab2;
 
-public class AirportEntry {
+import org.apache.hadoop.io.Writable;
+import org.mortbay.util.IO;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportEntry implements Writable{
+
+    public int getAirportID() {
+        return airportID;
+    }
+
+    private int airportID;
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    private  String airportName;
+    public final static int CODE = 0;
+    public final static int DESC = 1;
+
+    public AirportEntry(){
+
+    }
+    public AirportEntry(String string){
+        string = cutQuotes(string);
+        String[]params = string.split("\",\"");
+        String code = params[CODE];
+        String description = params[DESC];
+        airportID = Integer.parseInt(code);
+        airportName = description;
+    }
+
+    public String cutQuotes(String string){
+        return string.substring(1,string.length() - 1);
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(airportID);
+        dataOutput.writeUTF(airportName);
+    }
+
+    @Override
+    public void readFields (DataInput dataInput) throws IOException{
+        airportID = dataInput.readInt();
+        airportName = dataInput.readUTF();
+    }
+
+    @Override
+    public String toString() {
+        return airportID + "," + airportName;
+    }
 }
