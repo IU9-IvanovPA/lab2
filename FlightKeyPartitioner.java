826c8850b05165985207261e8c1d4ec1a1258385
Pavel Ivanov <funkpaws@MacBook-Pro-Pavel.local>
2019-10-20 15:36:46 +0300

    Scripted auto-commit on change (2019-10-20 15:36:46) by gitwatch.sh

--- FlightKeyPartitioner.java
+++ FlightKeyPartitioner.java
@@ -1,4 +1,11 @@
 package funkpaws.hadoop.lab2;
 
-public class FlightKeyPartitioner {
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.io.Text;
+
+public class FlightKeyPartitioner extends Partitioner<FlightKey, Text> {
+    @Override
+    public int getPartition(FlightKey flightKey, Text text, int i) {
+        return flightKey.getAirportId() % i;
+    }
 }
