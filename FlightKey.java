3162ceeb8824d32884eb9ec85510cc15130457d6
Pavel Ivanov <funkpaws@MacBook-Pro-Pavel.local>
2019-10-20 15:28:43 +0300

    Scripted auto-commit on change (2019-10-20 15:28:43) by gitwatch.sh

--- FlightKey.java
+++ FlightKey.java
@@ -2,6 +2,10 @@ package funkpaws.hadoop.lab2;
 
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class FlightKey implements WritableComparable<FlightKey> {
     public int getAirportId() {
         return airportId;
@@ -41,7 +45,23 @@ public class FlightKey implements WritableComparable<FlightKey> {
         FlightKey other = (FlightKey)obj;
         return (airportId == other.airportId && type == other.type);
     }
+    @Override public void write(DataOutput dataOutput) throws IOException{
+        dataOutput.writeInt(airportId);
+        dataOutput.writeInt(type);
+    }
 
     @Override
-    
+    public  void readFields(DataInput dataInput) throws IOException{
+        airportId = dataInput.readInt();
+        type = dataInput.readInt();
+    }
+
+    @Override
+    public String toString() {
+        return airportId + "," + type;
+    }
+
+    @Override public int hashCode() {
+        return toString().hashCode();
+    }
 }
